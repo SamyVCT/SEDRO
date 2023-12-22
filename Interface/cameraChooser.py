d450388aba99ee13d02b5b17649b667f9c1a9e2f
CorentinLa <corentin.lachevre@ensta-paris.fr>
2023-12-22 10:38:51 +0100

    Ajout de commentaires

--- Interface/cameraChooser.py
+++ Interface/cameraChooser.py
@@ -1,5 +1,6 @@
 import cv2
 
+# start webcam
 def init():
     global cap
     cap = cv2.VideoCapture(0)
@@ -13,12 +14,16 @@ def changeCamera(i):
     cap.set(3, 1920)
     cap.set(4, 1080)
     
-
+# Mise à jour des caméras disponibles pour pouvoir choisir une caméra branchée après le lancement du programme
 def updateCameras(menuCamera):
     menuCamera.delete(0, 'end')
     menuCamera.add_command(label="Update", command=lambda: updateCameras())
+    # Suppose qu'il n'y a pas plus de 5 caméras branchées
     for i in range(0, 5):
-        cam = cv2.VideoCapture(i)
+        try:
+            cam = cv2.VideoCapture(i)
+        except:
+            continue
         if cam.isOpened():
             menuCamera.add_command(label="Camera " + str(i), command=lambda c=i:changeCamera(c))
         cam.release()
