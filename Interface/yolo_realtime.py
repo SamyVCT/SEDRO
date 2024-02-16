6418a53cf2c1024e3043eb5cf60badf27333b016
CorentinLa <corentin.lachevre@ensta-paris.fr>
2024-02-16 15:49:30 +0100

    Affichage que de certaines classes voulues et traduction en français de celles-ci

--- Interface/yolo_realtime.py
+++ Interface/yolo_realtime.py
@@ -33,14 +33,14 @@ def yolo_realtime_boot(globImage):
     # Si ça marche pas peut être il faut installer CUDA 12.1 : https://developer.nvidia.com/cuda-toolkit
 
     # object classes
-    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+    classNames = ["personne", "vélo", "voiture", "moto", "aeroplane", "bus", "train", "camion", "boat",
                 "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
                 "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
                 "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
                 "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
                 "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
                 "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-                "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "téléphone",
                 "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
                 "teddy bear", "hair drier", "toothbrush"
                 ]
@@ -60,29 +60,36 @@ def yolo_realtime_boot(globImage):
             boxes = r.boxes
 
             for box in boxes:
-                # bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+                classWritten = ""
+                if cls < 3 or classNames[cls] == "téléphone":
+                    classWritten = classNames[cls]
+                elif cls == 4:
+                    classWritten = "personne (corrigé)"
+                
+                if classWritten != "":
+                    # bounding box
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-                # put box in cam
-                cv.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                    # put box in cam
+                    cv.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-                # confidence
-                confidence = math.ceil((box.conf[0]*100))/100
-                print("Confidence --->",confidence)
+                    # confidence
+                    confidence = math.ceil((box.conf[0]*100))/100
+                    #print("Confidence --->",confidence)
 
-                # class name
-                cls = int(box.cls[0])
-                print("Class name -->", classNames[cls])
+                    # class name
+                    cls = int(box.cls[0])
+                    #print("Class name -->", classNames[cls])
 
-                # object details
-                org = [x1, y1]
-                font = cv.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
+                    # object details
+                    org = [x1, y1]
+                    font = cv.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    color = (255, 0, 0)
+                    thickness = 2
 
-                cv.putText(img, classNames[cls] + " confiance : " + str(confidence), org, font, fontScale, color, thickness)
+                    cv.putText(img, classNames[cls] + " confiance : " + str(confidence), org, font, fontScale, color, thickness)
         globImage[1] = img
 
 
