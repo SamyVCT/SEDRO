6d88e8dd58fe2a876423d66bb4438604b2ee3130
CorentinLa <corentin.lachevre@ensta-paris.fr>
2024-04-26 15:06:35 +0200

    Auteurs

--- SEDRO/main.py
+++ SEDRO/main.py
@@ -1,3 +1,9 @@
+##################### Auteurs:  #####################
+# - LACHEVRE Corentin                               #
+# - VINCENT Samy                                    #
+# - HENRIQUE MARQUES GONCALVES Luiz                 #
+#####################################################
+
 from PIL import Image, ImageDraw, ImageTk
 import math
 import tkinter as tk
@@ -31,97 +37,6 @@ def resource_path(relative_path):
 ##############################################################################################################
 # Partie purement graphique de l'interface                                                                   #
 ##############################################################################################################
-
-
-# def polygonDirection(angle1, angle2, width, height):
-#     center = (width/2, height/2)
-
-#     angle1 = angle1%360
-#     angle2 = angle2%360
-    
-#     if(angle1 < 45 or angle1 >= 315):
-#         point1 = (width,  height/2 - width*math.tan(angle1*math.pi/180)/2)
-
-#     elif (angle1 < 135):
-#         point1 = (width/2 + height/(2*math.tan(angle1*math.pi/180)), 0)
-
-#     elif (angle1 < 225):
-#         point1 = (0, height/2 - width*math.tan(math.pi - angle1*math.pi/180)/2)
-
-#     elif (angle1 < 315):
-#         point1 = (width/2 - height*(math.tan(3*math.pi/2 - angle1*math.pi/180))/2, height)
-
-#     if(angle2 < 45 or angle2 >= 315):
-#         point2 = (width, height/2 - width*math.tan(angle2*math.pi/180)/2)
-
-#     elif (angle2 < 135):
-#         point2 = (width/2 + height/(2*math.tan(angle2*math.pi/180)), 0)
-
-#     elif (angle2 < 225):
-#         point2 = (0, height/2 - width*math.tan(math.pi - angle2*math.pi/180)/2)
-
-#     elif (angle2 < 315):
-#         point2 = (width/2 - height*(math.tan(3*math.pi/2 - angle2*math.pi/180))/2, height)
-
-
-#     point1 = (point1[0] + point1[0] - width/2, point1[1] + point1[1] - height/2)
-#     point2 = (point2[0] + point2[0]- width/2, point2[1] + point2[1] - height/2)
-
-#     return [(width/2, height/2), point1, point2]
-
-# def drawMap(color, angles, points, areas,mapPanel):
-#     im = Image.open(resource_path("basedrone2.png")).resize(map_size).convert('RGBA')
-#     width, height = im.width, im.height
-#     drone = Image.open(resource_path("drone.png")).resize((50,25)).convert('RGBA')
-#     d = ImageDraw.Draw(im)
-#     widthDrone, heightDrone = drone.size
-#     #print(angles[0][0], angles[0][1])
-    
-
-#     for angle in angles:
-#         d.polygon(polygonDirection(angle[0], angle[1], width, height), fill=color, outline=color)
-
-#     for x in points:
-#         tamanho_ponto = 10
-#         d.line((x[0] - tamanho_ponto, x[1] - tamanho_ponto, x[0] + tamanho_ponto, x[1] + tamanho_ponto), fill=x[2], width=5)
-#         d.line((x[0] + tamanho_ponto, x[1] - tamanho_ponto, x[0] - tamanho_ponto, x[1] + tamanho_ponto), fill=x[2],width=5)
-
-#     for area in areas:
-#         tamanho_ponto = area[2]
-#         d.ellipse((area[0] - tamanho_ponto, area[1] - tamanho_ponto, area[0] + tamanho_ponto, area[1] + tamanho_ponto), fill=area[3])
-
-
-#     # x_ponto = 3*width/4
-#     # y_ponto = 3*height/4
-#     # tamanho_ponto = 10
-#     # d.line((x_ponto - tamanho_ponto, y_ponto - tamanho_ponto, x_ponto + tamanho_ponto, y_ponto + tamanho_ponto), fill='red', width=5)
-#     # d.line((x_ponto + tamanho_ponto, y_ponto - tamanho_ponto, x_ponto - tamanho_ponto, y_ponto + tamanho_ponto), fill='red',width=5)
-
-#     im.paste(drone, (int(width/2 - widthDrone/2), int(height/2 - heightDrone/2)))
-
-#     photo = ImageTk.PhotoImage(im)
-
-#     mapPanel.configure(image=photo)
-#     mapPanel.image = photo
-
-# def MapLoop(mapPanel, im):
-#     a1 = 0
-#     a2 = 20
-
-#     a3 = 180
-#     a4 = 200
-
-#     x1 = 300
-#     y1 = 100
-#     x2 = 200
-#     y2 = 300
-
-#     while(1):
-#         drawMap('green', [(a1,a2), (a3,a4)], [(x1,y1,'blue'), (x2,y2, 'yellow')], [(300, 500, 50, '#f3000faa')],mapPanel)
-#         a1 += 1
-#         a2 += 1
-#         a3 += 1
-#         a4 += 1
 
 def printDetectionList(panel):
     T = tk.Text(panel, font=("Arial", 15), bg="#303030", fg="white",highlightthickness=0)
